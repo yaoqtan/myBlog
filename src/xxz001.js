27c505f14751e36a5495e1355895f7ec00cf349a
qintao.yao <qintan.yao@gtomato.com>
2018-12-18 01:10:55 +0800

    update

---
+++ src/xxz001.js
@@ -0,0 +1,13 @@
+import './less/xxz.less';
+import './less/xxz001.less';
+
+import Swiper from 'swiper';
+
+let swiper = new Swiper('.swiper-container', {
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    loop         : true,
+
+});
